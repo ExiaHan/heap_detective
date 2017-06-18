452cfe32757822fd78027964072ea593ac1def80
CoolerVoid <coolerlair@gmail.com>
2017-06-18 01:30:42 -0300

    fix bug at var_nmae

--- samplers/example6.c
+++ samplers/example6.c
@@ -3,13 +3,13 @@
 
 int main(void)
 {   
-	char *ch_ptr = NULL;
-	ch_ptr = malloc(100);
+	char *X = NULL;
+	X = malloc(100);
 	int i;
 
 	for (i = 0; i < 99; i++) 
 	{
-		ch_ptr = 'A';
+		X = 'A';
 		printf("%s\n", ch_ptr);
 	}
 
