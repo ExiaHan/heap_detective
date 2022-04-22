60f4dfbd55b8a4aa64bf88ffd2d4ecafa606a365
Your Name <coolerlair@gmail.com>
2022-04-21 23:57:51 -0300

    update

--- samplers/example3.c
+++ samplers/example3.c
@@ -5,12 +5,26 @@
 int main(int argc, char **argv) 
 {
 	int *array = NULL;
+        char *array3 = NULL;
 
 	array = new int[100];
+
 	delete [] array;
 
 	array=(int *)2;
    
+	short int x=0;
+
+	array3=realloc(400);
+
+	while(x!=10)
+	{
+		array3='1';
+		x++;
+		free(array3);
+	}
+
+	
 
 	return 0;
 }
